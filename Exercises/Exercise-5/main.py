0ae143910fd4450c4b51098fd5bd0563ae7712f7
sjrojanooo <sjrojano@gmail.com>
2022-03-15 12:36:34 -0700

    finished copying files into the database. Ill be implementing the indexes to boost query performance

--- Exercises/Exercise-5/main.py
+++ Exercises/Exercise-5/main.py
@@ -1,10 +1,12 @@
-import psycopg2; 
-import pandas as pd;
-from dotenv import dotenv_values;
+import psycopg2; # PostgreSql database adapter for python;
+import pandas as pd; # Pythong library for working with datasets; 
+from dotenv import dotenv_values; # loading all dotenv variables; 
 
+# creating a dictionary from all the variables located in my .env file; 
 config = {**dotenv_values('.env')}
 
 def main():
+
     # connection to postgres database 
     host=config['DATABASE_HOST']
     database=config['DATABASE_NAME']
@@ -49,7 +51,7 @@ def main():
 
     # sql script to create the tables that correspond to each csv file; 
     # moving this into another file later; 
-    accountsTable = """CREATE TABLE accounts(
+    accountsTable = """CREATE TABLE account(
             customer_id INTEGER, 
             first_name VARCHAR(50), 
             last_name VARCHAR(50),
@@ -60,11 +62,12 @@ def main():
             zip_code varchar(50), 
             join_date DATE,
             PRIMARY KEY(customer_id)
+            INDEX cust_idx ON (customer_id)
             )
         """
 
 
-    productsTable = """CREATE TABLE products(
+    productsTable = """CREATE TABLE product(
         product_id INTEGER, 
         product_code INTEGER,
         product_description VARCHAR(150), 
@@ -79,18 +82,41 @@ def main():
         product_description VARCHAR(150), 
         quantity INTEGER , 
         account_id INTEGER,
-        PRIMARY KEY(transactio_id),
-        FOREIGN KEY(product_id) REFERENCES products (product_id),
-        FOREIGN KEY(account_id) REFERENCES accounts (customer_id)
+        PRIMARY KEY(transaction_id),
+        FOREIGN KEY(product_id) REFERENCES product (product_id),
+        FOREIGN KEY(account_id) REFERENCES account (customer_id)
     ) """
 
 
+    # DDL statement to copy all csv data over to the database; 
+    insertAccounts = """copy account(customer_id, first_name, last_name, address_1,address_2, city, city_state, zip_code, join_date)\
+        FROM '/Users/stevenrojano/Data-Engineering-Exercises/Exercises/Exercise-5/data/accounts.csv' 
+        DELIMITER ',' 
+        CSV HEADER;"""
 
+    insertProducts= """copy product(product_id, product_code, product_description)\
+        FROM '/Users/stevenrojano/Data-Engineering-Exercises/Exercises/Exercise-5/data/products.csv' 
+        DELIMITER ',' 
+        CSV HEADER;"""
+
+    insertTransactions= """copy transaction(transaction_id, transaction_date, product_id, product_code,product_description, quantity, account_id)\
+        FROM '/Users/stevenrojano/Data-Engineering-Exercises/Exercises/Exercise-5/data/transactions.csv' 
+        DELIMITER ',' 
+        CSV HEADER;"""
+
+    # executing sql table creations;
     cursor.execute(accountsTable)
     cursor.execute(productsTable)
     cursor.execute(transactionTable)
-    
+
+    # copying data from csv files into the database; 
+    cursor.execute(insertAccounts)
+    cursor.execute(insertProducts)
+    cursor.execute(insertTransactions)
+
+    # commiting all executions;
     conn.commit()
+    # closing the connection; 
     conn.close()
 
 
