c378d471e08da43a806f0ce3cc9d427369eeb19a
sjrojanooo <sjrojano@gmail.com>
2022-03-14 18:29:33 -0700

    starting exercise 5. I am connected to the database and have create some DDL CREATE statements for my tables. I will be executing those with the cursor shortly

--- Exercises/Exercise-5/main.py
+++ Exercises/Exercise-5/main.py
@@ -1,13 +1,76 @@
-import pyscopg2
+import psycopg2; 
+import pandas as pd;
+from dotenv import dotenv_values;
+
+config = {**dotenv_values('.env')}
 
 def main():
-    host = 'postgres'
-    database='postgres'
-    user='postgres'
-    pas='postgres'
-    conn = pyscopg2.connect(host=host, database=database, user=user, password=pas)
 
-    # code goes here; 
+    # connection to postgres database 
+    host=config['POSTGRES_HOST']
+    database=config['POSTGRES_DB']
+    user=config['POSTGRES_USER']
+    pas=config['POSTGRES_PASSWORD']
+    conn = psycopg2.connect(host=host, database=database, user=user, password=pas)
+
+    # cursor will be used to create tables; 
+    cursor = conn.cursor()
+
+    # accounts data;
+    accountsDf = pd.read_csv('./data/accounts.csv', index_col='customer_id');
+
+    # products data;
+    productsDf = pd.read_csv('./data/products.csv', index_col='product_id');
+ 
+     # products data;
+    transactionsDf = pd.read_csv('./data/transactions.csv', index_col='transaction_id');
+
+    print('account column names')
+    print('---------------------') 
+    print(accountsDf.columns);
+
+    print('products column names') 
+    print('---------------------') 
+    print(productsDf.columns); 
+
+    print('transactions column names') 
+    print('---------------------') 
+    print(transactionsDf.columns)
+
+    # sql script to create the tables that correspond to each csv file; 
+    # moving this into another file later; 
+    accountsTable = """CREATE TABLE accounts(
+            customer_id INTEGER, 
+            first_name VARCHAR(50), 
+            last_name VARCHAR(50),
+            address_1 VARCHAR(100),
+            address_2 VARCHAR(100), 
+            city VARCHAR(50), 
+            city_state VARCHAR(50),
+            zip_code varchar(50), 
+            join_date DATE,
+            PRIMARY KEY(customer_id)
+            )
+        """
+
+
+    productsTable = """CREATE TABLE products(
+        product_id INTEGER, 
+        product_code INTEGER,
+        product_description VARCHAR(150), 
+        PRIMARY KEY(product_id)
+    ) """
+
+    transactionTable = """CREATE TABLE transaction(
+        transaction_id VARCHAR(200), 
+        transaction_date DATE,
+        product_id INTEGER, 
+        product_code INTEGER,
+        product_description VARCHAR(150), 
+        quantity INTEGER , 
+        account_id INTEGER,
+        PRIMARY KEY(transactio_id)
+    ) """
 
 
 if __name__ == '__main__':
