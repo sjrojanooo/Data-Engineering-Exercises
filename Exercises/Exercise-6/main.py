8fcbed328c41c50962c9c337b56106c243f93a51
sjrojanooo <sjrojano@gmail.com>
2022-03-17 14:28:36 -0700

    reading in csv files from zipfiles

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,11 +1,44 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession;
+import pandas as pd; 
+import zipfile; 
+import os; 
+import glob; 
 
+
+# method to walk the data directory and return the path endpoint to each zip file; 
+def walkDataDir(presentDirectory):
+
+    dataPath = f'{presentDirectory}/data'; 
+
+    zippedFiles = []; 
+
+    for root, dir_names ,fileNames in os.walk(dataPath):
+
+        for f in fileNames: 
+
+            zippedFiles.append(os.path.join(root,f))
+
+    return zippedFiles; 
 
 def main():
-    spark = SparkSession.builder.appName('Exercise6') \
-        .enableHiveSupport().getOrCreate()
-    # your code here
 
+    # spark = SparkSession.builder.appName('Exercise6') \
+    #     .enableHiveSupport().getOrCreate()
 
+    # print(spark)
+
+    zipResponse = walkDataDir(os.path.abspath(''))    
+
+    zip_file = zipfile.ZipFile(zipResponse[0])
+
+    print(zip_file.namelist()[0])
+
+    df = pd.read_csv(zip_file.open(zip_file.namelist()[0]))
+
+    print(df)
+
+    
+ 
+    pass
 if __name__ == '__main__':
     main()
