9582a1320f2c97dd3f0ec3d8281163e286939e62
sjrojanooo <sjrojano@gmail.com>
2022-03-20 18:45:51 -0700

    finished up 3 most popular stations by day. all with pyspark.sql dataframe concepts. finishing up the day with longest and shortest trips by gender

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -63,62 +63,50 @@ def readZipContents(zipIndex,csvIndex):
 def averageTripsAndTotalTrips(dataFrame):
 
     # formats date time to mm/dd/yyyy format; 
-    return dataFrame.withColumn("start_time",F.to_timestamp(
-        
-            "start_time").cast(
-                
-                "date")).groupBy("start_time").agg(F.count(dataFrame.trip_id).alias("total_trips"),
-                
-                    F.round(F.mean(dataFrame.tripduration)/2 ,2).alias("avg_trip_time")).orderBy("start_time")
+    return dataFrame.withColumn("start_time",F.to_timestamp("start_time").cast("date"))\
+            .groupBy("start_time")\
+                .agg(F.count(dataFrame.trip_id).alias("total_trips"),
+                    F.round(F.mean(dataFrame.tripduration)/2 ,2).alias("avg_trip_time"))\
+                        .orderBy("start_time")
 
 # returns most popular station by month; 
 def stationPopularityByMonth(dataFrame):
     # will be used to partition the result of the groupby statement. 
     w = Window().partitionBy("month").orderBy(F.desc("total_trips"))
 
-    return dataFrame.select("start_time","from_station_name","trip_id").withColumn("start_time", F.to_timestamp("start_time").cast("date")
-                
-                ).withColumn("month", F.date_format("start_time","MMMM")
-                    
-                    ).groupBy("from_station_name","month").agg(F.count("trip_id").alias("total_trips")
-                    
-                        ).withColumn("rank",F.dense_rank().over(w)).where(F.col("rank") == 1).select("month", "from_station_name","total_trips")
+    return dataFrame.select("start_time","from_station_name","trip_id")\
+            .withColumn("start_time", F.to_timestamp("start_time").cast("date"))\
+                .withColumn("month", F.date_format("start_time","MMMM"))\
+                    .groupBy("from_station_name","month").agg(F.count("trip_id").alias("total_trips"))\
+                        .withColumn("rank",F.dense_rank().over(w)).where(F.col("rank") == 1)
 
-
+# What were the top 3 trip stations each day for the last two weeks?
 def stationPopularityByDay(dataFrame):
-    # What were the top 3 trip stations each day for the last two weeks?
-    # formatting the date columns and formatting the day day name; 
-    dailyPopularity = dataFrame.copy().assign(
+    
+    w = Window().orderBy(F.lit(1)); 
+    # window to partition total trips by start_time 
+    # I am using dense_rank because rank will skip positios after any equal rankings; 
+    w2 = Window().partitionBy("start_time").orderBy(F.desc("total_trips"))
 
-                day_of_week=lambda day: (pd.to_datetime(dataFrame["start_time"]).dt.day_name())).loc[
-
-                    pd.to_datetime(dataFrame["start_time"]) >= pd.to_datetime(dataFrame["start_time"].max()) - pd.Timedelta(days=14)
-
-                        ].assign(
-
-                            start_time=lambda x: (pd.to_datetime(x["start_time"]).dt.strftime("%m/%d/%Y")))[
-                                
-                                ["start_time","day_of_week","from_station_name","trip_id"]].groupby([
-
-                                            "start_time","day_of_week","from_station_name"
-                                            
-                                                ], as_index=False).agg(
-
-                                                    total_trip_count=("trip_id","count")
-
-                                                        ).sort_values(
-                                                            
-                                                            ["start_time","total_trip_count"]).groupby("start_time").tail(3)
-    return create_frame(dailyPopularity)
+    return dataFrame.select("start_time","from_station_name","trip_id")\
+                    .withColumn("start_time", F.to_timestamp("start_time").cast("date"))\
+                        .withColumn("day_of_week", F.date_format("start_time","E"))\
+                            .groupBy("start_time","day_of_week","from_station_name")\
+                                .agg(F.count("trip_id").alias("total_trips"))\
+                                    .withColumn("max_date", F.max("start_time").over(w))\
+                                        .filter("start_time >= max_date - interval 14 days").drop("max_date")\
+                                            .withColumn("rank", F.dense_rank().over(w2))\
+                                                .where((F.col("rank") ==1)| (F.col("rank") ==2)| (F.col("rank") ==3))\
+                                                    .orderBy("start_time","rank")
 
 # Do Males or Females take longer trips on average?
 def averageTripDurationByGender(dataFrame):
     
-    return dataFrame.select("gender","tripduration").groupBy("gender").agg(
-        
-            F.round(F.mean("tripduration"),2).alias("avg_trip")).replace('NaN', None
+    return dataFrame.select("gender","tripduration")\
+        .groupBy("gender")\
+            .agg(F.round(F.mean("tripduration"),2).alias("avg_trip"))\
+                    .replace("NaN", None).dropna()
                 
-                ).dropna()
 # def longestShortestTripByAge(dataFrame):
 
     # # What is the top 10 ages of those that take the longest trips, and shortest?
@@ -165,13 +153,14 @@ def main():
 
     # What is the average trip duration per day?
     # How many trips were taken each day?
-    averageTotalTripsDf = averageTripsAndTotalTrips(quarter19DataFrame)
+    
+    # averageTotalTripsDf = averageTripsAndTotalTrips(quarter19DataFrame)
 
-    monthDf19 = stationPopularityByMonth(quarter19DataFrame); 
+    # monthDf19 = stationPopularityByMonth(quarter19DataFrame); 
 
     # dailyPopularity19 = stationPopularityByDay(quarter19DataFrame); 
 
-    tripDurationByGender = averageTripDurationByGender(quarter19DataFrame); 
+    # tripDurationByGender = averageTripDurationByGender(quarter19DataFrame); 
 
     # longestTripsByGender, shortestTripsByGender = longestShortestTripByAge(quarter19DataFrame); 
 
