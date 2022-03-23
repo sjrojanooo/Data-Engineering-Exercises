e2b9faad2e11b98d8c1ba16f67be44ff5ca14e71
sjrojanooo <sjrojano@gmail.com>
2022-03-22 17:44:52 -0700

    program now perform extract and transforming stages for both quarter 19 and quarter 20 files. Last step is automate the loading of the csv files to the reports directory

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -16,15 +16,7 @@ spark.conf.set("parquet.enable.summary-metadata", "false")
 # creates instance of sparkContext; 
 sc = spark.sparkContext; 
 
-# creates a pyspark data frame; 
-def create_frame(dataFrame):
-
-    # partition number used to split the large list from the csv file 
-    # into multiple small ones to reduce the message size; 
-    partitionNumber = 100; 
-
-    return spark.createDataFrame(sc.parallelize(dataFrame.values.tolist(),partitionNumber),dataFrame.columns.tolist())
-
+# BEGINS EXTRACT LEVEL; 
 # method to walk the data directory and return the path endpoint to each zip file; 
 def walkDataDir(presentDirectory):
 
@@ -58,19 +50,35 @@ def readZipContents(zipIndex,csvIndex):
         data_frame = pd.concat(datalist)
         
         return data_frame
-    
+
+# ENDS EXTRACT PHASE; 
+
+# BEGINS TRANSFORM PHASE; 
+# creates a pyspark data frame; 
+def create_frame(dataFrame):
+
+    # partition number used to split the large list from the csv file 
+    # into multiple small ones to reduce the message size; 
+    partitionNumber = 100; 
+
+    return spark.createDataFrame(sc.parallelize(dataFrame.values.tolist(),partitionNumber),dataFrame.columns.tolist())
+
 # discoverd average trip duration and trips taken each day; 
-def averageTripsAndTotalTrips(dataFrame):
+def averageTripsAndTotalTrips(dataFrame, startTimeCol, endTimeCol, tripIDCol):
 
     # 1. formats date time to mm/dd/yyyy format; 
     # 2. performs a group by on the start_time key, I removed the timestamp from them so that days would have the same format; 
     # 3. performs a count on the trip_id, rounds to two decimal places, and creates an alias or new new name for the tripduration column 
-    dataFrame = dataFrame.withColumnRenamed("start_time","date")\
-            .withColumn("date",F.to_timestamp("date").cast("date"))\
-                .groupBy("date")\
-                    .agg(F.count(dataFrame.trip_id).alias("total_trips"),
-                        F.round(F.mean(dataFrame.tripduration)/2,2).alias("avg_trip_time_in_minutes"))\
-                            .orderBy("date")
+    dataFrame = dataFrame.select(startTimeCol,endTimeCol, tripIDCol)\
+                    .withColumn("date",F.date_format(F.to_timestamp(startTimeCol),"MM-dd-yyyy"))\
+                        .withColumn(startTimeCol, F.to_timestamp(startTimeCol).cast(T.LongType()))\
+                            .withColumn(endTimeCol, F.to_timestamp(endTimeCol).cast(T.LongType()))\
+                                .withColumn("trip_in_minutes", F.round((F.col(endTimeCol) - F.col(startTimeCol))/60,2))\
+                                    .select("date","trip_in_minutes",tripIDCol)\
+                                        .groupBy("date")\
+                                            .agg(F.count(F.col(tripIDCol)).alias("total_trips"),
+                                                F.round(F.mean(F.col("trip_in_minutes")),2).alias("avg_trip_time_in_minutes"))\
+                                                    .orderBy("date")                    
     return dataFrame
 
 # returns most popular station by month; 
@@ -89,30 +97,30 @@ def stationPopularityByMonth(dataFrame, dateCol, stationCol, tripIDCol):
     return dataFrame
 
 # What were the top 3 trip stations each day for the last two weeks?
-def stationPopularityByDay(dataFrame):
+def stationPopularityByDay(dataFrame,dateCol, stationCol, tripIdCol):
 
-    # creates a dummy column and assigns a literal/constant value to it; 
-    w = Window().orderBy(F.lit(1)); 
     # window to partition total trips by start_time 
     # I am using dense_rank because rank will skip positios after any equal rankings; 
-    w2 = Window().partitionBy("start_time").orderBy(F.desc("total_trips"))
+    w = Window().partitionBy(dateCol).orderBy(F.desc("total_trips"))
 
-    dataFrame = dataFrame.select("start_time","from_station_name","trip_id")\
-                    .withColumn("start_time", F.to_timestamp("start_time").cast("date"))\
-                        .withColumn("day_of_week", F.date_format("start_time","E"))\
-                            .groupBy("start_time","day_of_week","from_station_name")\
-                                .agg(F.count("trip_id").alias("total_trips"))\
-                                    .withColumn("max_date", F.max("start_time").over(w))\
-                                        .filter("start_time >= max_date - interval 14 days").drop("max_date")\
-                                            .withColumn("rank", F.dense_rank().over(w2))\
-                                                .where((F.col("rank") <= 3))\
-                                                    .orderBy("start_time","rank")
+    # capturing the max date and using the date_sub function to return the date 14 days prior; 
+    max_date = dataFrame.select(F.date_sub(F.to_date(F.max(dateCol)),14).alias("max_date"))\
+                        .collect()[0]["max_date"]
+
+    dataFrame = dataFrame.select(dateCol,stationCol,tripIdCol)\
+                    .withColumn(dateCol, F.to_date(dateCol))\
+                        .withColumn("day_of_week", F.date_format(dateCol,"E"))\
+                            .groupBy(dateCol,"day_of_week",stationCol)\
+                                .agg(F.count(tripIdCol).alias("total_trips"))\
+                                    .where(F.col(dateCol) >= max_date)\
+                                        .withColumn("rank", F.dense_rank().over(w))\
+                                            .where((F.col("rank") <=3))\
+                                                .orderBy(dateCol, "rank")
 
     return dataFrame
 
 # Do Males or Females take longer trips on average?
 def averageTripDurationByGender(dataFrame):
-    # Females take the longest trips. 
 
     w = Window().partitionBy("gender").orderBy("tripduration")
 
@@ -137,32 +145,39 @@ def longestShortestTripByAge(dataFrame):
     # Top 10 longest trips by age; 
     longest = dataFrame.where(F.col("rank") <= 10)
     # Top 10 shortest trips by age; 
-    shortest = dataFrame.filter(F.col("rank") >= dataFrame.count() - 9).sort(F.desc("rank"))
+    shortest = dataFrame.filter(F.col("rank") > dataFrame.count() - 10).sort(F.desc("rank"))
 
     return  longest, shortest
 
+# ENDS TRANSFORM PHASE; 
+
 def main():    
 
-    # Quarter 19 contents; 
-    quarter19DataFrame = create_frame(readZipContents(1,0))
+    # # Quarter 19 contents; 
+    quarter19DataFrame = create_frame(readZipContents(1,0));
 
-    averageTotalTripsDf = averageTripsAndTotalTrips(quarter19DataFrame)
+    q19AverageTotalTripsDf = averageTripsAndTotalTrips(quarter19DataFrame, "start_time","end_time","trip_id");
 
-    q19MonthlyPopularityDf = stationPopularityByMonth(quarter19DataFrame,"start_time", "from_station_name","trip_id")
+    q19MonthlyPopularityDf = stationPopularityByMonth(quarter19DataFrame,"start_time", "from_station_name","trip_id");
 
-    dailyPopularity19 = stationPopularityByDay(quarter19DataFrame)
+    dailyPopularity19 = stationPopularityByDay(quarter19DataFrame,"start_time", "from_station_name", "trip_id");
 
-    tripDurationByGender = averageTripDurationByGender(quarter19DataFrame)
+    tripDurationByGender = averageTripDurationByGender(quarter19DataFrame);
 
-    longestTrips, shortestTrips= longestShortestTripByAge(quarter19DataFrame)
-    #ENDS QUARTER 19 DIVVY TRIP INFO; 
+    longestTrips, shortestTrips= longestShortestTripByAge(quarter19DataFrame);
+    # #ENDS QUARTER 19 DIVVY TRIP INFO; 
 
-    # Quarter 20 contents; 
+    # # Quarter 20 contents; 
     quarter20DataFrame = create_frame(readZipContents(0,0))
 
-    q20MonthlyPopularityDf = stationPopularityByMonth(quarter20DataFrame, "started_at", "start_station_name", "ride_id")
+    q20AverageTotalTripsDf = averageTripsAndTotalTrips(quarter20DataFrame, "started_at","ended_at","ride_id");
 
-    q20MonthlyPopularityDf.show()
+    q20MonthlyPopularityDf = stationPopularityByMonth(quarter20DataFrame, "started_at", "start_station_name", "ride_id");
+
+    q20DailyPopularityDf = stationPopularityByDay(quarter20DataFrame, "started_at", "start_station_name", "ride_id");
+
+    q20DailyPopularityDf.show();
+    #ENDS QUARTER 20 DIVVY TRIP INFO; 
 
 
 if __name__ == "__main__":
